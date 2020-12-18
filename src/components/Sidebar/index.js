4092f0a46e374260adf063643e8ee4b79487afbd
vy3191 <venkyafhs@gmail.com>
2020-12-17 21:47:06 -0500

    Applied styles to the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,9 +4,11 @@ import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => (
     <nav className="components-sidebar">
-      <NavLink activeClassName="active" exact replace to="/home">Home</NavLink>
-      <NavLink activeClassName="active" exact replace to="/about">About</NavLink>
-      <NavLink activeClassName="active" exact replace to="/contact">Contact</NavLink>
+      <ul>
+        <li><NavLink activeClassName="active" exact replace to="/home">Home</NavLink></li>
+        <li><NavLink activeClassName="active" exact replace to="/about">About</NavLink></li>
+        <li><NavLink activeClassName="active" exact replace to="/contact">Contact</NavLink></li>
+      </ul>
     </nav>
 );
 
