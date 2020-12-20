c97296cb4267e70310a06b07971bc571ccb4e7fc
vy3191 <venkyafhs@gmail.com>
2020-12-20 12:25:10 -0500

    Created input fields and modified className also

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,11 +3,17 @@
 const Input = ({ type, className, id, name, onChange }) => {
   const [ focus, setFocus ] = React.useState(false);
 
+  const getClassName = () => [
+      'components-input',
+      focus && 'focus',
+      className
+  ].filter(v => v).join(" ")
+
+
   return (
-    <div className={`components-input${focus && ' focus' || ''}${className && ` ${className}` ||''} `}>
+    <div className={ getClassName() }>
       <input 
         type={ type }
-        className ={ className }
         id={ id }
         name={ name || id }
         onChange = { onChange }
