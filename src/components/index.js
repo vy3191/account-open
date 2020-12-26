01350968818a6013613140018ba9818edeb1007a
vy3191 <venkyafhs@gmail.com>
2020-12-26 12:38:49 -0500

    Fixed webpack error

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Sidebar } from './Sidebar';
-export { default as AOField } from './AOField';
+export { default as AOInput } from './AOInput';
 export { default as AOButton } from './AOButton';
