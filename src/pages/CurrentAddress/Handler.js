70302caf814ebfa9150e574bb3ec51081da40dfd
vy3191 <venkyafhs@gmail.com>
2021-01-06 13:00:15 -0500

    Added service call to current address component

--- src/pages/CurrentAddress/Handler.js
+++ src/pages/CurrentAddress/Handler.js
@@ -1,14 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { saveApplication} from '../../redux/actions';
 import CurrentAddressContainer from './Container'
 
-function CurrentAddressDetails() {
+const CurrentAddressDetails = (props) => {
+  const { applicationData: {id }} = useSelector(state => state.applicationReducer),
+                      dispatch = useDispatch();
+
   const onSubmit = values => {
-    console.log('values>>>', values)
- }
+    const payload = { currentAddress: values};
+    dispatch(saveApplication(id, payload, handleRoute));
+ };
+
+ const handleRoute = () => {
+   props.history.push("/monthly-income");
+ };
+
   return (
     <div>
       <CurrentAddressContainer onSubmit={ onSubmit }/>
     </div>
-  )
-}
+  );
+};
 
 export default CurrentAddressDetails;
