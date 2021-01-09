9e70c24e8185859af72156689b8407e7f0c59bfc
vy3191 <venkyafhs@gmail.com>
2021-01-09 14:20:59 -0500

    Modified some card styles

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,12 +2,16 @@ import { BsPencilSquare } from 'react-icons/bs';
 import { Link } from 'react-router-dom'
 
 const Card = ({ data: {title, editPath, ...rest} })  => {
-  console.log('rest>>>>', rest);
   return (
     <article className="components-card">
       <h2>{title}<Link className="edit" to={ editPath }><BsPencilSquare /></Link></h2>
       <div className="wrapper">
-        
+        { 
+          /* item = [key, value] */
+          Object.entries(rest).map(([key, value], idx) => (
+            <p><strong>{key}</strong>&nbsp;{value}</p>
+          ))
+        }
   
       </div>
     </article>
