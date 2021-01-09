16d1d9fa219cd5396b8908cf7ee3b4d68b8e7070
vy3191 <venkyafhs@gmail.com>
2021-01-09 14:07:22 -0500

    Updated the card component with title and edit icon

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,21 @@
+import { BsPencilSquare } from 'react-icons/bs';
+import { Link } from 'react-router-dom'
 
-
-function Card(props) {
+const Card = ({ data: {title, editPath, ...rest} })  => {
+  console.log('rest>>>>', rest);
   return (
     <article className="components-card">
-      { props.children }
+      <h2>{title}<Link className="edit" to={ editPath }><BsPencilSquare /></Link></h2>
+      <div className="wrapper">
+        
+  
+      </div>
     </article>
   )
+};
+
+Card.propTypes = {
+  data: PropTypes.object
 }
 
 export default Card
